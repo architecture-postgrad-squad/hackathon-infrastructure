31a734feafbc73cc5c1d840242f069e67a7174f3
Eduardo Petri <eduardo.petri@senior.com.br>
2025-02-10 21:30:39 -0300

    [fix] fix k8s infraestructure

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -19,20 +19,31 @@ resource "aws_subnet" "hackathon_subnet_1" {
   }
 }
 
+resource "aws_subnet" "hackathon_subnet_2" {
+  vpc_id            = aws_vpc.hackathon_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "hackathon-subnet-2"
+  }
+}
+
 resource "aws_eks_cluster" "hackathon_eks" {
   name     = "hackathon-cluster"
-  role_arn = "arn:aws:iam::083022795774:role/LabRole"
+  role_arn = "arn:aws:iam::925162579276:role/LabRole"
 
   vpc_config {
-    subnet_ids = [aws_subnet.hackathon_subnet_1.id]
+    subnet_ids = [aws_subnet.hackathon_subnet_1.id, aws_subnet.hackathon_subnet_2.id]
   }
 }
 
 resource "aws_eks_node_group" "snack_bar_worker_group" {
   cluster_name    = aws_eks_cluster.hackathon_eks.name
   node_group_name = "hackathon-worker-group"
-  node_role_arn   = "arn:aws:iam::083022795774:role/LabRole"
-  subnet_ids      = [aws_subnet.hackathon_subnet_1.id]
+  node_role_arn   = "arn:aws:iam::925162579276:role/LabRole"
+  subnet_ids = [aws_subnet.hackathon_subnet_1.id, aws_subnet.hackathon_subnet_2.id]
 
   scaling_config {
     desired_size = 2
